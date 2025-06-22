711116759e6fcacdb07960b33ab0abf0d6492b28
unknown <tarasovlev7@gmail.com>
2025-06-22 23:26:50 +0300

    chat-place

--- src/components/ui/WorkSpace/WorkSpace.tsx
+++ src/components/ui/WorkSpace/WorkSpace.tsx
@@ -1,11 +1,64 @@
-
 import React from 'react';
-import { Box } from '@mui/material';
+import {
+    Avatar,
+    Box,
+    ListItemAvatar,
+    ListItemButton,
+    ListItemText,
+    Typography,
+    alpha,
+} from '@mui/material';
 
 const WorkSpace = () => {
     return (
-        <Box sx={{ display: 'flex', width: "100%",height: '96vh',  borderRadius: "12px",backgroundColor: '#FFFFFF', }}>
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                width: '100%',
+                height: '96vh',
+                borderRadius: '12px',
+                backgroundColor: '#FFFFFF',
+                padding: '20px',
+                boxSizing: "border-box"
+            }}
+        >
+            <ListItemButton
+                sx={{
+                    width: '100%',
+                    height: '50px',
+                    padding: 0,
+                    maxHeight: '50px',
 
+
+                }}
+            >
+                <ListItemAvatar>
+                    <Avatar />
+                </ListItemAvatar>
+                <ListItemText
+                    primary={
+                        <Typography noWrap>
+                            TL
+                        </Typography>
+                    }
+                    secondary={
+                        <Typography noWrap color="text.secondary">
+                            20.02.2025
+                        </Typography>
+                    }
+                />
+            </ListItemButton>
+
+            <Box
+                sx={{
+                    width: '100%',
+                    height: '100%',
+                    backgroundColor: '#F6F6F6',
+                    borderRadius: "12px",
+                    mt: 2,
+                }}
+            />
         </Box>
     );
 };
